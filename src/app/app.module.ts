f1044822250c2805465a872facda7f31ca386f5d
ahmed <ahmednas_alani@yahoo.com>
2020-09-11 10:49:39 -0700

    conect our component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,8 @@ import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component
 import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoopingEditComponent } from './shopping-list/shooping-edit/shooping-edit.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
 
 
 @NgModule({
@@ -21,7 +22,8 @@ import { ShoopingEditComponent } from './shopping-list/shooping-edit/shooping-ed
     RecipeDetailComponent,
     RecipeItemComponent,
     ShoppingListComponent,
-    ShoopingEditComponent,
+    ShoppingEditComponent,
+    
    
   ],
   imports: [
--- src/app/shopping-list/shopping-edit/shopping-edit.component.spec.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ShoopingEditComponent } from './shooping-edit.component';
+import { ShoppingEditComponent } from './shopping-edit.component';
 
-describe('ShoopingEditComponent', () => {
-  let component: ShoopingEditComponent;
-  let fixture: ComponentFixture<ShoopingEditComponent>;
+describe('ShoppingEditComponent', () => {
+  let component: ShoppingEditComponent;
+  let fixture: ComponentFixture<ShoppingEditComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ShoopingEditComponent ]
+      declarations: [ ShoppingEditComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ShoopingEditComponent);
+    fixture = TestBed.createComponent(ShoppingEditComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
